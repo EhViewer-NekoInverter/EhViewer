671968b7d5382a5f14680698d19f9aac222a9e2b
小白-白 <i@moedog.org>
2023-03-14 12:20:07 +0800

    Kotlinize CheckTextView

--- app/src/main/java/com/hippo/widget/CheckTextView.kt
+++ app/src/main/java/com/hippo/widget/CheckTextView.kt
@@ -13,119 +13,97 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.hippo.widget
 
-package com.hippo.widget;
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import androidx.appcompat.widget.AppCompatCheckedTextView
+import com.hippo.ehviewer.R
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.util.AttributeSet;
-import android.view.Gravity;
-import android.view.View;
+open class CheckTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatCheckedTextView(context, attrs, defStyleAttr), View.OnClickListener {
+    private val mSelfBounds = Rect()
+    private val mOverlayBounds = Rect()
+    private var mForegroundInPadding = true
+    private var mForegroundBoundsChanged = false
+    private var mForeground: Drawable? = null
+    private var mForegroundGravity = Gravity.FILL
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.AppCompatCheckedTextView;
-
-import com.hippo.ehviewer.R;
-
-public class CheckTextView extends AppCompatCheckedTextView implements View.OnClickListener {
-    private final Rect mSelfBounds = new Rect();
-    private final Rect mOverlayBounds = new Rect();
-    protected boolean mForegroundInPadding = true;
-    boolean mForegroundBoundsChanged = false;
-    private Drawable mForeground;
-    private int mForegroundGravity = Gravity.FILL;
-
-    public CheckTextView(Context context) {
-        this(context, null);
-    }
-
-    public CheckTextView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public CheckTextView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-
-        init(context, attrs, defStyleAttr);
-    }
-
-    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CheckTextView,
-                defStyleAttr, 0);
-
+    init {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.CheckTextView,
+            defStyleAttr, 0
+        )
         mForegroundGravity = a.getInt(
-                R.styleable.CheckTextView_android_foregroundGravity, mForegroundGravity);
-
-        Drawable d = a.getDrawable(R.styleable.CheckTextView_android_foreground);
-        if (d != null) {
-            setForeground(d);
+            R.styleable.CheckTextView_android_foregroundGravity, mForegroundGravity
+        )
+        a.getDrawable(R.styleable.CheckTextView_android_foreground)?.let {
+            foreground = it
         }
-
         mForegroundInPadding = a.getBoolean(
-                R.styleable.CheckTextView_foregroundInsidePadding, true);
-
-        a.recycle();
-        setOnClickListener(this);
+            R.styleable.CheckTextView_foregroundInsidePadding, true
+        )
+        a.recycle()
+        setOnClickListener(this)
     }
 
     /**
      * Describes how the foreground is positioned.
      *
      * @return foreground gravity.
-     * @see #setForegroundGravity(int)
+     * @see .setForegroundGravity
      */
-    public int getForegroundGravity() {
-        return mForegroundGravity;
+    override fun getForegroundGravity(): Int {
+        return mForegroundGravity
     }
 
     /**
      * Describes how the foreground is positioned. Defaults to START and TOP.
      *
-     * @param foregroundGravity See {@link android.view.Gravity}
-     * @see #getForegroundGravity()
+     * @param foregroundGravity See [android.view.Gravity]
+     * @see .getForegroundGravity
      */
-    public void setForegroundGravity(int foregroundGravity) {
-        if (mForegroundGravity != foregroundGravity) {
-            if ((foregroundGravity & Gravity.RELATIVE_HORIZONTAL_GRAVITY_MASK) == 0) {
-                foregroundGravity |= Gravity.START;
+    override fun setForegroundGravity(foregroundGravity: Int) {
+        var sForegroundGravity = foregroundGravity
+        if (mForegroundGravity != sForegroundGravity) {
+            if (sForegroundGravity and Gravity.RELATIVE_HORIZONTAL_GRAVITY_MASK == 0) {
+                sForegroundGravity = sForegroundGravity or Gravity.START
             }
-
-            if ((foregroundGravity & Gravity.VERTICAL_GRAVITY_MASK) == 0) {
-                foregroundGravity |= Gravity.TOP;
+            if (sForegroundGravity and Gravity.VERTICAL_GRAVITY_MASK == 0) {
+                sForegroundGravity = sForegroundGravity or Gravity.TOP
             }
-
-            mForegroundGravity = foregroundGravity;
-
+            mForegroundGravity = sForegroundGravity
             if (mForegroundGravity == Gravity.FILL && mForeground != null) {
-                Rect padding = new Rect();
-                mForeground.getPadding(padding);
+                val padding = Rect()
+                mForeground!!.getPadding(padding)
             }
-
-            requestLayout();
+            requestLayout()
         }
     }
 
-    @Override
-    protected boolean verifyDrawable(@NonNull Drawable who) {
-        return super.verifyDrawable(who) || (who == mForeground);
+    override fun verifyDrawable(who: Drawable): Boolean {
+        return super.verifyDrawable(who) || who === mForeground
     }
 
-    @Override
-    public void jumpDrawablesToCurrentState() {
-        super.jumpDrawablesToCurrentState();
+    override fun jumpDrawablesToCurrentState() {
+        super.jumpDrawablesToCurrentState()
         if (mForeground != null) {
-            mForeground.jumpToCurrentState();
+            mForeground!!.jumpToCurrentState()
         }
     }
 
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        if (mForeground != null && mForeground.isStateful()) {
-            mForeground.setState(getDrawableState());
+    override fun drawableStateChanged() {
+        super.drawableStateChanged()
+        if (mForeground != null && mForeground!!.isStateful) {
+            mForeground!!.state = drawableState
         }
     }
 
@@ -135,8 +113,8 @@ public class CheckTextView extends AppCompatCheckedTextView implements View.OnCl
      *
      * @return A Drawable or null if no foreground was set.
      */
-    public Drawable getForeground() {
-        return mForeground;
+    override fun getForeground(): Drawable {
+        return mForeground!!
     }
 
     /**
@@ -147,86 +125,74 @@ public class CheckTextView extends AppCompatCheckedTextView implements View.OnCl
      *
      * @param drawable The Drawable to be drawn on top of the children.
      */
-    public void setForeground(Drawable drawable) {
-        if (mForeground != drawable) {
-            if (mForeground != null) {
-                mForeground.setCallback(null);
-                unscheduleDrawable(mForeground);
+    override fun setForeground(drawable: Drawable?) {
+        if (mForeground !== drawable) {
+            mForeground?.let {
+                it.callback = null
+                unscheduleDrawable(it)
             }
-
-            mForeground = drawable;
-
+            mForeground = drawable
             if (drawable != null) {
-                setWillNotDraw(false);
-                drawable.setCallback(this);
-                if (drawable.isStateful()) {
-                    drawable.setState(getDrawableState());
+                setWillNotDraw(false)
+                drawable.callback = this
+                if (drawable.isStateful) {
+                    drawable.state = drawableState
                 }
                 if (mForegroundGravity == Gravity.FILL) {
-                    Rect padding = new Rect();
-                    drawable.getPadding(padding);
+                    val padding = Rect()
+                    drawable.getPadding(padding)
                 }
             } else {
-                setWillNotDraw(true);
+                setWillNotDraw(true)
             }
-            requestLayout();
-            invalidate();
+            requestLayout()
+            invalidate()
         }
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        mForegroundBoundsChanged |= changed;
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        mForegroundBoundsChanged = mForegroundBoundsChanged or changed
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mForegroundBoundsChanged = true;
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mForegroundBoundsChanged = true
     }
 
-    @Override
-    public void draw(@NonNull Canvas canvas) {
-        super.draw(canvas);
-
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
         if (mForeground != null) {
-            final Drawable foreground = mForeground;
-
+            val foreground: Drawable = mForeground!!
             if (mForegroundBoundsChanged) {
-                mForegroundBoundsChanged = false;
-                final Rect selfBounds = mSelfBounds;
-                final Rect overlayBounds = mOverlayBounds;
-
-                final int w = getRight() - getLeft();
-                final int h = getBottom() - getTop();
-
+                mForegroundBoundsChanged = false
+                val selfBounds = mSelfBounds
+                val overlayBounds = mOverlayBounds
+                val w = right - left
+                val h = bottom - top
                 if (mForegroundInPadding) {
-                    selfBounds.set(0, 0, w, h);
+                    selfBounds[0, 0, w] = h
                 } else {
-                    selfBounds.set(getPaddingLeft(), getPaddingTop(),
-                            w - getPaddingRight(), h - getPaddingBottom());
+                    selfBounds[paddingLeft, paddingTop, w - paddingRight] = h - paddingBottom
                 }
-
-                Gravity.apply(mForegroundGravity, foreground.getIntrinsicWidth(),
-                        foreground.getIntrinsicHeight(), selfBounds, overlayBounds);
-                foreground.setBounds(overlayBounds);
+                Gravity.apply(
+                    mForegroundGravity, foreground.intrinsicWidth,
+                    foreground.intrinsicHeight, selfBounds, overlayBounds
+                )
+                foreground.bounds = overlayBounds
             }
-
-            foreground.draw(canvas);
+            foreground.draw(canvas)
         }
     }
 
-    @Override
-    public void drawableHotspotChanged(float x, float y) {
-        super.drawableHotspotChanged(x, y);
+    override fun drawableHotspotChanged(x: Float, y: Float) {
+        super.drawableHotspotChanged(x, y)
         if (mForeground != null) {
-            mForeground.setHotspot(x, y);
+            mForeground!!.setHotspot(x, y)
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        setChecked(!isChecked());
+    override fun onClick(v: View) {
+        isChecked = !isChecked
     }
 }
