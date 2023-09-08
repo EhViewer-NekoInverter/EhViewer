6fc779f79626e424aa8601f1896c1aee2f05f67f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-09-08 15:36:28 +0800

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    kotlin("android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
+    id("com.android.application") version "8.1.1" apply false
+    kotlin("android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
 
 allprojects {
@@ -9,7 +9,7 @@ allprojects {
 }
 
 tasks.register("Delete", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 buildscript {
