d9fddfeb7be6dd3eb5ec649c94f1464d59189994
vvb2060 <vvb2060@gmail.com>
2024-01-12 23:19:40 +0800

    Replace hosts.txt with DoH

--- app/src/main/java/com/hippo/ehviewer/EhApplication.kt
+++ app/src/main/java/com/hippo/ehviewer/EhApplication.kt
@@ -31,6 +31,7 @@ import coil.util.DebugLogger
 import com.hippo.ehviewer.client.EhCookieStore
 import com.hippo.ehviewer.client.EhDns
 import com.hippo.ehviewer.client.EhEngine
+import com.hippo.ehviewer.client.EhSSLSocketFactory
 import com.hippo.ehviewer.client.EhTagDatabase
 import com.hippo.ehviewer.client.data.GalleryDetail
 import com.hippo.ehviewer.coil.MergeInterceptor
@@ -47,6 +48,7 @@ import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import kotlinx.coroutines.DelicateCoroutinesApi
 import okhttp3.Cache
 import okhttp3.OkHttpClient
+import okhttp3.internal.platform.Platform
 import java.io.File
 
 class EhApplication : SceneApplication(), ImageLoaderFactory {
@@ -222,6 +224,7 @@ class EhApplication : SceneApplication(), ImageLoaderFactory {
                 cookieJar(EhCookieStore)
                 dns(EhDns)
                 proxySelector(ehProxySelector)
+                sslSocketFactory(EhSSLSocketFactory(), Platform.get().platformTrustManager())
                 addInterceptor(CloudflareInterceptor(application))
             }.build()
         }
---
+++ app/src/main/java/com/hippo/ehviewer/client/EhSSLSocketFactory.kt
@@ -0,0 +1,63 @@
+package com.hippo.ehviewer.client
+
+import com.hippo.ehviewer.Settings
+import java.net.InetAddress
+import java.net.Socket
+import javax.net.ssl.SNIHostName
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocket
+import javax.net.ssl.SSLSocketFactory
+
+class EhSSLSocketFactory : SSLSocketFactory() {
+    private val factory: SSLSocketFactory
+
+    init {
+        val context = SSLContext.getInstance("TLS")
+        context.init(null, null, null)
+        factory = context.socketFactory
+    }
+
+    override fun createSocket(s: Socket?, host: String?, port: Int, autoClose: Boolean): Socket {
+        val socket = factory.createSocket(s, host, port, autoClose) as SSLSocket
+        if (!Settings.doH) return socket
+        val params = socket.sslParameters
+        params.serverNames = listOf(SNIHostName("eh"))
+        socket.sslParameters = params
+        return socket
+    }
+
+    override fun createSocket(host: String?, port: Int): Socket {
+        return factory.createSocket(host, port)
+    }
+
+    override fun createSocket(
+        host: String?,
+        port: Int,
+        localHost: InetAddress?,
+        localPort: Int
+    ): Socket {
+        return factory.createSocket(host, port, localHost, localPort)
+    }
+
+    override fun createSocket(host: InetAddress?, port: Int): Socket {
+        return factory.createSocket(host, port)
+    }
+
+    override fun createSocket(
+        address: InetAddress?,
+        port: Int,
+        localAddress: InetAddress?,
+        localPort: Int
+    ): Socket {
+        return factory.createSocket(address, port, localAddress, localPort)
+    }
+
+    override fun getDefaultCipherSuites(): Array<String> {
+        return factory.defaultCipherSuites
+    }
+
+    override fun getSupportedCipherSuites(): Array<String> {
+        return factory.supportedCipherSuites
+    }
+
+}
