8c3c42930d2e6b6ae1052ceddffb5393e9757776
小白-白 <i@moedog.org>
2026-03-19 22:07:26 +0800

    Optimize WebView version retrieval

--- app/src/main/java/com/hippo/ehviewer/util/WebViewExtensions.kt
+++ app/src/main/java/com/hippo/ehviewer/util/WebViewExtensions.kt
@@ -1,13 +1,29 @@
 package com.hippo.ehviewer.util
 
 import android.annotation.SuppressLint
+import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.webkit.WebViewCompat
 import com.hippo.ehviewer.EhApplication
 import com.hippo.ehviewer.Settings
 
 private const val MINIMUM_WEBVIEW_VERSION = 118
-val WebViewVersion = WebViewCompat.getCurrentWebViewPackage(EhApplication.application)?.versionName?.substringBefore('.')?.toIntOrNull() ?: MINIMUM_WEBVIEW_VERSION
+val WebViewVersion = run {
+    val context = EhApplication.application
+    val uaVersion = runCatching {
+        Regex("Chrome/(\\d+)")
+            .find(WebSettings.getDefaultUserAgent(context))
+            ?.groupValues
+            ?.get(1)
+            ?.toIntOrNull()
+    }.getOrNull()
+    val pkgVersion = WebViewCompat
+        .getCurrentWebViewPackage(context)
+        ?.versionName
+        ?.substringBefore('.')
+        ?.toIntOrNull()
+    uaVersion ?: pkgVersion ?: MINIMUM_WEBVIEW_VERSION
+}
 val isWebViewOutdated = WebViewVersion < MINIMUM_WEBVIEW_VERSION
 
 @SuppressLint("SetJavaScriptEnabled")
